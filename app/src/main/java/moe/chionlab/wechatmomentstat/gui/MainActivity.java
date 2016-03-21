3f54109aa7dc40d25f19435b32409f293fb2a4ad
Chion Tang <sdspeedonion@gmail.com>
2016-03-21 20:08:10 +0800

    Async loading

--- app/src/main/java/moe/chionlab/wechatmomentstat/gui/MainActivity.java
+++ app/src/main/java/moe/chionlab/wechatmomentstat/gui/MainActivity.java
@@ -1,6 +1,7 @@
 package moe.chionlab.wechatmomentstat.gui;
 
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,21 +30,40 @@ public class MainActivity extends AppCompatActivity {
         ((Button)findViewById(R.id.launch_button)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                try {
-                    ((Button)findViewById(R.id.launch_button)).setText(R.string.exporting_sns);
-                    task.copySnsDB();
-                    task.initSnsReader();
-                    task.snsReader.run();
-                    ((Button)findViewById(R.id.launch_button)).setText(R.string.launch);
-                } catch (Throwable e) {
-                    Toast.makeText(MainActivity.this, R.string.not_rooted, Toast.LENGTH_LONG).show();
-                    Log.e("wechatmomentstat", "exception", e);
-                }
+                ((Button) findViewById(R.id.launch_button)).setText(R.string.exporting_sns);
+                ((Button) findViewById(R.id.launch_button)).setEnabled(false);
+                new RunningTask().execute();
             }
         });
 
+    }
 
+    class RunningTask extends AsyncTask<Void, Integer, Long> {
+
+        Throwable error = null;
+
+        @Override
+        protected Long doInBackground(Void... params) {
+            try {
+                task.copySnsDB();
+                task.initSnsReader();
+                task.snsReader.run();
+            } catch (Throwable e) {
+                this.error = e;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            super.onPostExecute(aLong);
+            ((Button)findViewById(R.id.launch_button)).setText(R.string.launch);
+            ((Button) findViewById(R.id.launch_button)).setEnabled(true);
+            if (this.error != null) {
+                Toast.makeText(MainActivity.this, R.string.not_rooted, Toast.LENGTH_LONG).show();
+                Log.e("wechatmomentstat", "exception", this.error);
+            }
+        }
     }
 
 
